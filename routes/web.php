639eaabd1b1d76484a66c6eab005e6070c3ad397
Marcel finavaro <marcelfinavaro@gmail.com>
2025-11-18 08:11:11 -0300

    forçando deploy na branch login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// teste
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
