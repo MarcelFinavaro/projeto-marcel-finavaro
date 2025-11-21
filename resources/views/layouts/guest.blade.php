49025a89d840181896dc6fc32c691969cf9127d0
Marcel finavaro <marcelfinavaro@gmail.com>
2025-11-21 08:13:31 -0300

    fix: replace vite directive with manual asset links for production

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       <!-- Scripts -->
+        <link href="{{ asset('build/assets/app-kUNBWEmv.css') }}" rel="stylesheet">
+        <script src="{{ asset('build/assets/app-kGY04szw.js') }}" defer></script>
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
